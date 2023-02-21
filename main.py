e1b52941fba6851e2e2a29d621dd16174792f25d
Danilosv337 <danilo_santos.vieira@hotmail.com>
2023-02-20 23:24:25 -0300

    using flake8 and mypy

--- main.py
+++ main.py
@@ -4,26 +4,25 @@ from email.message import EmailMessage
 
 env = dotenv_values('.env')
 
-class Email: 
+
+class Email:
     def __init__(self):
-        self.email_send : str = env['email_send']
-        self.email_destiny : str = env['email_destiny']
-        self.password : str = env['email_password']
+        self.email_send: str = env['email_send']
+        self.email_destiny: str = env['email_destiny']
+        self.password: str = env['email_password']
 
-
-    def send_email(self):
+    def send_email(self, assunto: str, mensagem: str) -> None:
         msg = EmailMessage()
-        msg['Subject'] : str = 'Assunto'
-        msg['From'] : str = self.email_send
-        msg['To'] : str =  self.email_destiny
-        msg.set_content('mensagem teste')
+        msg['Subject'] = assunto
+        msg['From'] = self.email_send
+        msg['To'] = self.email_destiny
+        msg.set_content(mensagem)
 
-        with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-            smtp.login(self.email_send,self.password)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(self.email_send, self.password)
             smtp.send_message(msg)
-
 
 
 if __name__ == '__main__':
     email = Email()
-    email.send_email()
+    email.send_email('teste', 'testando')
